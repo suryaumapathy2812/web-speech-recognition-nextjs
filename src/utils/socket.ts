332ab722af308a211cb1ea57337c05401b0846a6
Surya Umapathy <suryaumapathy2812@gmail.com>
2023-11-24 17:36:40 +0530

    Refactor socket initialization code

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,21 +1,22 @@
 
 import { io, Socket } from 'socket.io-client';
 
+const webSocketEndPoint = process.env.NEXT_PUBLIC_WEB_SOCKET_ENDPOINT as string;
 let socket: Socket;
 
 if (process.env.NODE_ENV === 'production') {
-  socket = io(process.env.NEXT_PUBLIC_WEB_SOCKET_ENDPOINT as string, {
+  console.log(process.env);
+  socket = io(webSocketEndPoint, {
     transports: ['websocket'],
     upgrade: false,
   });
-
 } else {
   const globalWithSocket = global as typeof globalThis & {
     socket: Socket;
   };
 
   if (!globalWithSocket.socket) {
-    globalWithSocket.socket = io(process.env.NEXT_PUBLIC_WEB_SOCKET_ENDPOINT as string, {
+    globalWithSocket.socket = io(webSocketEndPoint, {
       transports: ['websocket'],
       upgrade: false,
     });
