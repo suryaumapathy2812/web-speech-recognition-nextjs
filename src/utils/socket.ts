6105a3933da1689bec66a350c91bb3ab1379d9ec
Surya Umapathy <suryaumapathy2812@gmail.com>
2023-11-24 17:18:25 +0530

    Update socket configuration

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -3,22 +3,19 @@ import { io, Socket } from 'socket.io-client';
 
 let socket: Socket;
 
-
 if (process.env.NODE_ENV === 'production') {
-
-  socket = io(process.env.WEB_SOCKET_ENDPOINT as string, {
+  socket = io(process.env.NEXT_PUBLIC_WEB_SOCKET_ENDPOINT as string, {
     transports: ['websocket'],
     upgrade: false,
   });
 
 } else {
-
   const globalWithSocket = global as typeof globalThis & {
     socket: Socket;
   };
 
   if (!globalWithSocket.socket) {
-    globalWithSocket.socket = io(process.env.WEB_SOCKET_ENDPOINT as string, {
+    globalWithSocket.socket = io(process.env.NEXT_PUBLIC_WEB_SOCKET_ENDPOINT as string, {
       transports: ['websocket'],
       upgrade: false,
     });
