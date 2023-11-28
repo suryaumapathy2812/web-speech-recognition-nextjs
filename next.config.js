2fba0844b640913365d5de7a492a5174600a8a8a
Surya Umapathy <suryaumapathy2812@gmail.com>
2023-11-28 12:17:04 +0530

    Update next.config.js and manifest.json

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 
-const withPWA = require('next-pwa')({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  // disable: process.env.NODE_ENV === 'development'
-});
+const withPWA = require('next-pwa');
 
-const nextConfig = {}
+const nextConfig = {
+  ...withPWA({
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  })
+}
 
-module.exports = withPWA(nextConfig)
+module.exports = nextConfig
